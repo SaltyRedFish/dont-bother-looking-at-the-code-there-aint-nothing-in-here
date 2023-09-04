e5fa0b6891efed7517c4860ccb11644ba45979f8
srfvidov <fkor3@AMK.cpq>
2023-09-04 09:55:58 +0800

    Initial Commit

---
+++ this is a placeholder/this_is_a_placeholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  this_is_a_placeholderApp.swift
+//  this is a placeholder
+//
+//  Created by T Krobot on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct this_is_a_placeholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
